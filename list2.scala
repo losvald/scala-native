cf72ec0b6bd6de185e2612ca4a9e74dc977bd096
Leo Osvald <leo.osvald@gmail.com>
2018-03-19 16:30:28 -0400

    Rewrite list2 in CPS then use 2nd-class lists

--- list2.scala
+++ list2.scala
@@ -1,28 +1,45 @@
-def isShorterThan(x@: List[Any], y@: List[Any]): Boolean = {
-  var xTail@ = x.head :: x.tail;
-  var yTail@ = y.head :: y.tail;
-
-  while (!yTail.isEmpty && !xTail.isEmpty) {
-    xTail = xTail.tail;
-    yTail = yTail.tail
-  };
-
-  !yTail.isEmpty && xTail.isEmpty
+def isShorterThan@(x@: List[Any@], y@: List[Any@]): Boolean = {
+  if (!y.isEmpty && !x.isEmpty) isShorterThan(x.tail, y.tail)
+  else !y.isEmpty && x.isEmpty
 };
 
-def tail(x@: List[Any], y@: List[Any], z@: List[Any]): List[Any] = {
+def withTail@[U](x@: List[Any@], y@: List[Any@], z@: List[Any@],
+  f@: List@[Any@] => U
+): U = {
   if (isShorterThan(y, x)) {
-    tail(tail(x.tail, y, z), tail(y.tail, z, x), tail(z.tail, x, y))
+    withTail[U](x.tail, y, z, {def _1@(x2@: List[Any@]) = {
+      withTail[U](y.tail, z, x, {def _2@(y2@: List[Any@]) = {
+        withTail[U](z.tail, x, y, {def _3@(z2@: List[Any@]) = {
+          withTail[U](x2, y2, z2, f)
+        }; _3})
+      }; _2})
+    }; _1})
   } else {
-    z.head :: z.tail
+    f(z)
   }
 };
 
-def makeList(len@: Int): List[Any] = listTabulate[Any](len, (i: Int) => i);
+def withList@[U](len@: Int, f@: List@[Any@] => U): U = {
+  def rec@[U](len@: Int, lst@: List[Int@], f@: List@[Any@] => U): U = {
+    if (len == 0) f(lst)
+    else rec[U](len - 1, len :: lst, f)
+  };
+  rec[U](len, Nil, f)
+};
 
-def run(xLen@: Int, yLen@: Int, zLen@: Int): Int = {
-  val result@ = tail(makeList(xLen), makeList(yLen), makeList(zLen));
-  listLength[Any](result)
+def listLength2@[T](l@: List[T@]): Int = // unlike listLength, use "T @local"
+  if (l.isEmpty) 0 else 1 + listLength2[T](l.tail);
+
+def run@(xLen@: Int, yLen@: Int, zLen@: Int): Int = {
+  withList[Int](xLen, {def _1@(x@: List[Any@]) = {
+    withList[Int](yLen, {def _2@(y@: List[Any@]) = {
+      withList[Int](zLen, {def _3@(z@: List[Any@]) = {
+        withTail[Int](x, y, z, {def _4@(result@: List[Any@]) = {
+          listLength2[Any](result)
+        }; _4})
+      }; _3})
+    }; _2})
+  }; _1})
 };
 
 val result@ = run(15, 10, 6);
